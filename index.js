763c480ff701d107a5b980ca37c768601a4b79f3
Yash Sinha <yashsinha7696@gmail.com>
2020-10-24 18:52:27 +0530

    Complete Drum Kit project added.

---
+++ index.js
@@ -0,0 +1,67 @@
+//Detecting button press
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for(let i=0; i< numberOfDrumButtons; i++){
+
+    document.querySelectorAll("button")[i].addEventListener("click", function (){
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+    });
+}
+
+//Detecting keyboard press
+document.addEventListener("keypress", function (event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key){
+    switch (key){
+        case "w" :
+            const tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "a" :
+            const tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s" :
+            const tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "d" :
+            const tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "j" :
+            const snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+
+        case "k" :
+            const crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+        case "l" :
+            const kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+
+        default:
+            console.log(this.innerHTML);
+    }
+}
+
+function buttonAnimation(key){
+    var activeButton = document.querySelector("." + key);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
